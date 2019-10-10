099b485aecb722313e8451781eb07dc736474baf
nurjandi <amirabdillah46@gmail.com>
2019-10-10 14:33:48 +0700

    push 1

---
+++ Stay-Away-or-I-ll-Shoot-You-master/Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+	public GameObject mainMenuHolder;
+		
+	public void Play() {
+		SceneManager.LoadScene ("Game");
+	}
+
+	public void Quit() {
+		Application.Quit ();
+	}
+
+	public void MainMenu() {
+		mainMenuHolder.SetActive (true);
+	}
+
+}
